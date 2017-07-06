4e8153e32d845bfd2e9a7e7d38684df714395e43
John <john.wong.h@gmail.com>
2017-07-06 00:21:02 +0000

    Added login logout features on all pages

--- express_server.js
+++ express_server.js
@@ -1,11 +1,13 @@
 var express = require("express");
+var cookieParser = require('cookie-parser');
 var app = express();
+app.use(cookieParser());
 var PORT = process.env.PORT || 8080; // default port 8080
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -31,12 +33,13 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase}
+  let templateVars = { urls: urlDatabase, username: req.cookies.username}
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {username: req.cookies.username}
+  res.render("urls_new", templateVars);
 });
 
 app.post("/urls", (req, res) => {
@@ -55,7 +58,11 @@ app.post("/urls", (req, res) => {
 
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
+  let templateVars = { 
+    shortURL: req.params.id, 
+    longURL: urlDatabase[req.params.id], 
+    username: req.cookies.username
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -89,10 +96,20 @@ app.get("/u/:shortURL", (req, res) => {
 app.post ("/urls/:id/delete", (req,res) => {
   delete urlDatabase[req.params.id];
   res.redirect('/urls')
-})
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body.username)
+  res.redirect('/urls')
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls')
 });
 
 app.listen(PORT, () => {
